1b2db322cce4118ece6af98dee3f8428275eec19
bananon877 <ayanofukuda0513@gmail.com>
2024-05-17 04:00:59 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,12 +4,11 @@ document.getElementById('playButton').addEventListener('click', function() {
 
     // 音楽を再生する
     audio.play();
-    // ボタンのテキストを「Traffic Paradise !」に変更
-    playButton.textContent = 'Traffic Paradise !';
+    // ボタンのテキストを「Ohhhh Yuki Tsunoda」に変更
+    playButton.textContent = 'Ohhhh Yuki Tsunoda';
 
-    // 音楽が再生終了したときにボタンのテキストを「再生」に戻す
+    // 音楽が再生終了したときにボタンのテキストを「声で応援」に戻す
     audio.addEventListener('ended', function() {
-        playButton.textContent = '再生';
+        playButton.textContent = '声で応援';
     });
 });
-
